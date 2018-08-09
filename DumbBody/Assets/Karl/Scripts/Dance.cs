ca1b5c1077da1d4bde267e1638da7dce474af1bb
SkytronUniverse <boatright.karl@gmail.com>
2018-08-09 14:59:44 -0400

    Slight changes to dance script, added body color on start

--- DumbBody/Assets/Karl/Scripts/Dance.cs
+++ DumbBody/Assets/Karl/Scripts/Dance.cs
@@ -5,30 +5,53 @@ using UnityEngine;
 public class Dance : MonoBehaviour {
 
     private readonly float speed = 10.0f;
-    private readonly float moveAngle = 15.0f;
+    private readonly float moveAngle = 45.0f;
 
     public static Dance instance = null;
     public GameObject[] joints;
 
+    public Quaternion[] musicOffPositions;
+
     private bool musicPlaying = true;
+
+    bool needsReset = false;
+
+    public Transform body;
 
 	void Awake () {
         instance = this; //Singleton
 	}
-
+    private void Start()
+    {
+        body = transform.Find("Torso");
+        for(int i = 0; i < 4; i++)
+        {
+            musicOffPositions[i] = joints[i].transform.rotation;
+        }
+    }
     private void Update()
     {
         if(SequencerAudio_v1.count > 0)
         {
             MakeDance();
+            needsReset = true;
+        }
+        else if(SequencerAudio_v1.count==0 && needsReset)
+        {
+            Debug.Log("Music is off");
+            for (int i = 0; i < 4; i++)
+            {
+                joints[i].transform.rotation = musicOffPositions[i];
+            }
+            needsReset = false;
         }
     }
 
     public void MakeDance()
     {
-        joints[0].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Sin(Time.time * speed), 0f, moveAngle * Mathf.Sin(Time.time * speed));
-        joints[1].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Sin(Time.time * speed),0f, moveAngle * Mathf.Cos(Time.time * speed));
-        joints[2].transform.rotation = Quaternion.Euler((moveAngle*3) * Mathf.Cos(Time.time * speed), (moveAngle * 3) * Mathf.Sin(Time.time * speed),  0f);
-        joints[3].transform.rotation = Quaternion.Euler((moveAngle * 3) * Mathf.Sin(Time.time * speed), (moveAngle * 3) * Mathf.Sin(Time.time * speed), 0f);
+        joints[0].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Sin(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed));
+        joints[1].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Sin(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed));
+        joints[2].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Cos(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed),  0f);
+        joints[3].transform.rotation = Quaternion.Euler(moveAngle * Mathf.Sin(Time.time * speed), moveAngle * Mathf.Sin(Time.time * speed), 0f);
     }
 }
